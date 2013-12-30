ad525ba25bac05e9e315e4a9e503933759cb6873
Leonardo Taccari <floor51@gmail.com>
2013-12-30 20:07:36 +0100

    Add compactor and reflector

--- TopDownBetter.py
+++ TopDownBetter.py
@@ -9,12 +9,19 @@ Approach:
 
 import os
 import csv
+from matplotlib import cm
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 SLEIGH_LENGTH = 1000
-MAX_LAYERS = 999999
+#MAX_LAYERS = 4 
+MAX_LAYERS = 999999 
+PLOT = False
+WRITE = True
 
 # Plotting
 xpos, ypos, zpos, dx, dy, dz = [],[],[],[],[],[]
+colors = []
 
 class Layer:
    """ Object to keep track of present position and max extent in sleigh so far. """    
@@ -37,13 +44,6 @@ class Layer:
       present.zpos = min(z1,z2)
       self.presents.append(present)
       self.z_max = max(self.z_max, present.zpos + present.z_depth - 1)
-      if self.id <= 2:
-         xpos.append(min(x1,x2))
-         ypos.append(min(y1,y2))
-         zpos.append(min(z1,z2))
-         dx.append(abs(x1-x2))
-         dy.append(abs(y1-y2))
-         dz.append(abs(z1-z2))
       return True
 
 
@@ -51,6 +51,67 @@ class Layer:
    def next_shelf(self):
       return
 
+   """ Copy presents and sort them decreasingly by tallest z-point they reach (if no compactor, the z_pos term is all the same (z_base), but otherwise it's not). """
+   def sort_presents(self):
+      self.sorted_presents = list(self.presents)
+      self.sorted_presents.sort(key= lambda p : (p.zpos+p.z_depth-1), reverse=True)
+      return self.sorted_presents
+     
+   """ Compactor """
+   def compact(self, prev_layer):
+      #prev_layer.z_base # do not exceed
+      #print "Layer", self.id
+      z_min = prev_layer.z_base-1 #z_min up to current present. then, following presents must not be smaller, they can be equal or greater.
+
+      # recompute z_max
+      self.z_max = self.z_base
+      
+      for p in self.presents: #in order of id!
+         # presents in the previous layer sorted by decreasing z-height
+         for up_p in prev_layer.sort_presents():
+            if p.overlap(up_p):
+               # this is the tallest overlapping present: you can move down the present, if possible. 
+               # if not possible, break!
+               diff = p.zpos - (up_p.zpos+up_p.z_depth)
+               if diff > 0 and (p.zpos - diff) >= z_min:
+#                  print "good overlap", p.id, diff
+#                  print "previous pos", p.zpos
+                  p.zpos -= diff
+#                  print "new pos", p.zpos
+                  z_min = max(p.zpos, z_min)
+               break
+         z_min = max(p.zpos, z_min)
+         #print "zmin", z_min
+         self.z_max = max(self.z_max, p.zpos + p.z_depth - 1)
+      
+      return
+
+
+   """ Finalize shelf """
+   def finalize_shelf(self):
+      #fracs = offset.astype(float)/offset.max()
+      #norm = colors.normalize(fracs.min(), fracs.max())
+      for i,p in enumerate(self.presents):
+         if self.id <= MAX_LAYERS:
+            x1, x2, y1, y2, z1, z2 = p.xpos, p.xpos+p.width-1, p.ypos, p.ypos + p.height-1, p.zpos, p.zpos + p.z_depth-1
+            xpos.append(min(x1,x2))
+            ypos.append(min(y1,y2))
+            zpos.append(min(z1,z2))
+            dx.append(abs(x1-x2))
+            dy.append(abs(y1-y2))
+            dz.append(abs(z1-z2))
+            colors.append(cm.jet(float(i)/len(self.presents)))
+
+   """ Reflect x and y axis """ 
+   def reflect_shelf(self):
+      #print "Flipping layer", self.id
+      for p in self.presents:
+         p.xpos = 1 + SLEIGH_LENGTH - (p.xpos + p.width - 1)
+         p.ypos = 1 + SLEIGH_LENGTH - (p.ypos + p.height - 1)
+         
+
+
+   """ Write next line """
    def write_shelf(self, writer, maxz):
       for p in self.presents:
          x1, x2, y1, y2, z1, z2 = p.xpos, p.xpos+p.width-1, p.ypos, p.ypos + p.height-1, p.zpos, p.zpos + p.z_depth-1
@@ -88,6 +149,21 @@ class Present:
    # rotate on the x-y plane
    def rotate(self):
       self.width, self.height = self.height, self.width
+      # should also change positions? it's not univocal.
+
+   def overlap(self, present):
+      # make sure that positions are good, not 0's!
+
+      if (self.xpos+self.width-1) < present.xpos:
+         return False
+      if self.xpos > (present.xpos+present.width-1):
+         return False
+      if (self.ypos+self.height-1) < present.ypos:
+         return False
+      if self.ypos > (present.ypos+present.height-1): 
+         return False
+      return True
+      
 
 class Node:
    def __init__(self):
@@ -113,7 +189,10 @@ class Node:
             return None
         
          # if the space is too small
+#         if self.width < present.width or self.height < present.height:
+#            present.rotate()
          if self.width < present.width or self.height < present.height:
+#            present.rotate() #rotate it back
             return None
 
          # if the space is perfect
@@ -187,7 +266,7 @@ if __name__ == "__main__":
     
    path = '.'
    presentsFilename = os.path.join(path, 'presents.csv')
-   submissionFilename = os.path.join(path, 'maxrectrotate.csv')
+   submissionFilename = os.path.join(path, 'test.csv')
    
    # create header for submission file: PresentId, x1,y1,z1, ... x8,y8,z8
    header = ['PresentId']
@@ -214,18 +293,22 @@ if __name__ == "__main__":
          
          if not packed_present:
             #print layer.id, present.id, float(present.id)/layer.id
-            # compact shelf upward (if possible), preserving order
-            #layer.compact(prev_layer)
+            if layer.id % 2 == 0:
+               layer.reflect_shelf()
+            # compact shelf down (if possible), preserving order
+            if prev_layer is not None:
+               layer.compact(prev_layer)
             
-            # write to file
-            #layer.write_shelf(writer)
+            # store coordinates for plotting
+            if PLOT:
+               layer.finalize_shelf()
             
             # open new shelf and add current present
             cumul_area = 0
             prev_layer = layer
-            layer = Layer(prev_layer.id+1, prev_layer.z_max+1)
-            if layer.id > MAX_LAYERS:
+            if layer.id >= MAX_LAYERS:
                break
+            layer = Layer(prev_layer.id+1, prev_layer.z_max+1)
             packed_present = layer.add_present(present)
 
          if not packed_present:
@@ -235,60 +318,64 @@ if __name__ == "__main__":
 
    print "Max z =", maxz
 
-   import matplotlib.pyplot as plt
-   from mpl_toolkits.mplot3d import Axes3D
-   fig = plt.figure()
-   ax = fig.add_subplot(111, projection='3d')
-   ax.bar3d(xpos,ypos,zpos,dx,dy,dz,color='r')
-   ax.set_xlim3d(0, 1000)
-   ax.set_ylim3d(0, 1000)
-   print "Plotting"
-   #plt.show()
+   if PLOT:
+      fig = plt.figure()
+      ax = fig.add_subplot(111, projection='3d')
+      ax.bar3d(xpos,ypos,zpos,dx,dy,dz,color=colors)
+      #ax.bar3d(xpos,ypos,zpos,dx,dy,dz,color='r')
+      ax.set_xlim3d(0, 1000)
+      ax.set_ylim3d(0, 1000)
+      print "Plotting"
+      plt.show()
 
-
-   print "Writing file"
-   layer = Layer(1,1)
-   prev_layer = None
-   with open(presentsFilename, 'rb') as f:
-      with open(submissionFilename, 'wb') as w:
-         f.readline() # header
-         fcsv = csv.reader(f)
-         wcsv = csv.writer(w)
-         wcsv.writerow(header)
-         
-         cumul_area = 0
-         for row in fcsv:
-            if int(row[0])%10000 == 0:
-              print row[0]
-
-            present = Present(row)
+   if WRITE:
+      print "Writing file"
+      layer = Layer(1,1)
+      prev_layer = None
+      with open(presentsFilename, 'rb') as f:
+         with open(submissionFilename, 'wb') as w:
+            f.readline() # header
+            fcsv = csv.reader(f)
+            wcsv = csv.writer(w)
+            wcsv.writerow(header)
             
-            packed_present = False
-            if cumul_area + present.area < SLEIGH_LENGTH*SLEIGH_LENGTH:
-               cumul_area += present.area
-               packed_present = layer.add_present(present) 
-            
-            if not packed_present:
-               #print layer.id, present.id, float(present.id)/layer.id
-               # compact shelf upward (if possible), preserving order -> can be done TODO
-               #layer.compact(prev_layer)
-               
-               # write to file
-               layer.write_shelf(wcsv, maxz)
-               
-               # open new shelf and add current present
-               cumul_area = 0
-               prev_layer = layer
-               layer = Layer(prev_layer.id+1, prev_layer.z_max+1)
-               if layer.id > MAX_LAYERS:
-                  break
-               packed_present = layer.add_present(present)
+            cumul_area = 0
+            for row in fcsv:
+               if int(row[0])%10000 == 0:
+                 print row[0]
 
-            if not packed_present:
-               print "Something wrong"
+               present = Present(row)
+               
+               packed_present = False
+               if cumul_area + present.area < SLEIGH_LENGTH*SLEIGH_LENGTH:
+                  cumul_area += present.area
+                  packed_present = layer.add_present(present) 
+               
+               if not packed_present:
+                  #print layer.id, present.id, float(present.id)/layer.id
+                  if layer.id % 2 == 0:
+                     layer.reflect_shelf()
 
-         # last layer has not been emptied!
-         layer.write_shelf(wcsv,maxz)
+                  # compact shelf down (if possible), preserving order 
+                  if prev_layer is not None:
+                     layer.compact(prev_layer)
+                  
+                  # write to file
+                  layer.write_shelf(wcsv, maxz)
+                  
+                  # open new shelf and add current present
+                  cumul_area = 0
+                  prev_layer = layer
+                  if layer.id >= MAX_LAYERS:
+                     break
+                  layer = Layer(prev_layer.id+1, prev_layer.z_max+1)
+                  packed_present = layer.add_present(present)
+
+               if not packed_present:
+                  print "Something wrong"
+
+            # last layer has not been emptied!
+            layer.write_shelf(wcsv,maxz)
 
 
    print 'Done'
